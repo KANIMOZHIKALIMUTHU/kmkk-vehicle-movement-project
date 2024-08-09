e31d42465718ba86d6073878a509894633eaf3b7
KANIMOZHIKALIMUTHU <kmkk2303@gmail.com>
2024-08-10 01:11:44 +0530

    Vehicle Management

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     const fetchVehicleData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/vehicle-data');
+        const response = await axios.get('https://kmkk-vehicle-movement-project.onrender.com/api/vehicle-data');
         if (Array.isArray(response.data)) {
           setPathData(response.data);
         } else {
